f8abf4a6692bc3ec90deaa853bc1aa3879c72232
taesung <ktsung94@naver.com>
2020-04-28 12:23:36 +0900

    FoodList 만들어 보기

---
+++ project-ex07/src/FoodListItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class FoodListItem extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+        <li>{ this.props.name } : { this.props.quantity }</li>
+        );
+    }
+}
